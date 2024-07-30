3393ea951cf4fdeca6eb84733168358616586efe
Rootless-137 <rootless137@gmail.com>
2024-07-30 23:59:08 +0100

    Atualizar o

---
+++ Scan tool.py
@@ -0,0 +1,26 @@
+import socket
+import os
+
+
+os.system('clear')
+
+
+print("""\n
+
+
+
+_____ _           _              _       
+ |  ___| |         | |            | |      
+ | |__ | |_ __ __ _| |_ ___  _ __| |_ ___ 
+ |  __|| | '__/ _` | __/ _ \| '__| __/ __|
+ | |___| | | | (_| | || (_) | |  | |_\__ \
+ \____/_|_|  \__,_|\__\___/|_|   \__|___/
+                                         
+Autor: Rootless137
+Aviso: Não sou responsável pelo mau uso desta ferramenta.
+
+
+
+\n""")
+
+host = str(input("Insira o alvo: "))
