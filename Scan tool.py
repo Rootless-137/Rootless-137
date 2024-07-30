16d791cd7ef9de36ed7a0182644721cc6d4f1bcf
Rootless-137 <rootless137@gmail.com>
2024-07-31 00:14:53 +0100

    Atualizar o Scan tool.py

--- Scan tool.py
+++ Scan tool.py
@@ -5,22 +5,33 @@ import os
 os.system('clear')
 
 
-print("""\n
+RED = "\033[91m"
+NEON_PURPLE = "\033[95m"
+RESET = "\033[0m"
 
+# Logo e considerações com cores
+print(f""" \n
+{RED}
 
-
-_____ _           _              _       
- |  ___| |         | |            | |      
- | |__ | |_ __ __ _| |_ ___  _ __| |_ ___ 
- |  __|| | '__/ _` | __/ _ \| '__| __/ __|
- | |___| | | | (_| | || (_) | |  | |_\__ \
- \____/_|_|  \__,_|\__\___/|_|   \__|___/
+  ___  ___  ___  ___  _    ___  ___  ___ 
+ | . \| . || . ||_ _|| |  | __>/ __>/ __>
+ |   /| | || | | | | | |_ | _> \__ \\__ \
+ |_\_\`___'`___' |_| |___||___><___/<___/
                                          
+
+{RESET}
+{NEON_PURPLE}
 Autor: Rootless137
 Aviso: Não sou responsável pelo mau uso desta ferramenta.
-
-
+{RESET}
 
 \n""")
 
+print(logo)
+
+ports = [22,21,20,443,445,80,8080,9009,9991,7070,3333,4040]
+
 host = str(input("Insira o alvo: "))
+
+for port in ports:
+     
