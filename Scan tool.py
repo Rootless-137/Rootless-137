1290bb465e95ac8562fde50593a4a6c94f5adac6
Rootless-137 <rootless137@gmail.com>
2024-07-31 01:08:10 +0100

    Atualizar o Scan tool.py

--- Scan tool.py
+++ Scan tool.py
@@ -1,15 +1,16 @@
+#!/usr/bin/env python3
 import socket
+import argparse
 import os
-
 
 os.system('clear')
 
-
+# Cores para o terminal
 RED = "\033[91m"
 NEON_PURPLE = "\033[95m"
 RESET = "\033[0m"
 
-
+# Cabeçalho
 print(f""" \n
 {RED}
 
@@ -27,26 +28,29 @@ Aviso: Não sou responsável pelo mau uso desta ferramenta.
 
 \n""")
 
+parser = argparse.ArgumentParser(description='Ferramenta de escaneamento de portas.')
+parser.add_argument('host', type=str, help='O endereço IP ou nome do host para escanear.')
+
+args = parser.parse_args()
+host = args.host
 
 ports = [22,21,20,443,445,80,8080,9009,9991,7070,3333,4040]
 
+# Contadores de portas abertas e fechadas
 r = 0
 i = 0
 
-host = str(input("Insira o alvo: "))
-
 print(f"\n{RED} ! O SCAN ESTÁ EM ANDAMENTO ! {RESET}\n")
 
-
 for port in ports:
-     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     client.settimeout(0.5)
-     code = client.connect_ex((host, port))
-     if code == 0:
-         i = i + 1
-         print(f" -> {port} ", "  || OPEN °")
-     else:
-         r = r + 1
-         print(f" -> {port} ", "  || CLOSED •")
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(0.5)
+    code = client.connect_ex((host, port))
+    if code == 0:
+        i += 1
+        print(f" -> {port} ", "  || OPEN °")
+    else:
+        r += 1
+        print(f" -> {port} ", "  || CLOSED •")
      
 print(f"\n{NEON_PURPLE} O SCAN ENCONTROU {i} PORTAS ABERTAS e {r} FECHADAS {RESET}")
